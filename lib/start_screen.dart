409379447b3da82c206a67193d99eeb1b9973a4f
galuhbudhiswara <galuhbudhiswara280705@gmail.com>
2023-05-12 16:47:09 +0700

    first commit

---
+++ lib/start_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(133, 255, 255, 255),
+          ),
+          
+          // Opacity(
+          //   opacity: 0.5,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png',
+          //     width: 300,
+          //   ),
+          // ),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter The Fun Way !',
+            style: TextStyle(
+              color: Color.fromARGB(255, 212, 176, 234),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed:
+              startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: Text("Start Quiz"),
+          )
+        ],
+      ),
+    );
+  }
+}
